0773de6f26b804495fa44e76d38118ea04a86b03
Daniel7 <dl4041569@gmail.com>
2026-03-19 12:52:35 -0500

    Agrego validaciones de documento y teléfono al formulario de aprendiz

--- SENA_APP/aprendices/forms.py
+++ SENA_APP/aprendices/forms.py
@@ -2,11 +2,24 @@ from django import forms
 from .models import Aprendiz
 
 class AprendizForm(forms.ModelForm):
-    # Clase Meta que vincula el formulario con el modelo y los campos de la BD
     class Meta:
         model = Aprendiz
         fields = ['documento_identidad', 'nombre', 'apellido', 'telefono', 'correo', 'fecha_nacimiento', 'ciudad', 'programa']
-        # Opcional: Puedes añadir widgets para mejorar la apariencia (ej. selectores de fecha)
         widgets = {
             'fecha_nacimiento': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
+
+    def clean_documento_identidad(self):
+        documento = self.cleaned_data.get('documento_identidad')
+        if not documento.isdigit():
+            raise forms.ValidationError('El documento solo admite solo numeros.')
+        return documento
+
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+        if telefono:  # es opcional, solo validar si fue ingresado
+            if not telefono.isdigit():
+                raise forms.ValidationError('El teléfono solo debe contener números.')
+            if len(telefono) != 10:
+                raise forms.ValidationError('El teléfono debe tener exactamente 10 dígitos.')
+        return telefono
